c296d951d09b5c73ae8f88ce7902f6a3a1617f8d
Radu Constantinescu <Constantinescu-3@student.tudelft.nl>
2023-05-08 13:57:32 +0200

    Refactor Workflow Module

--- modules/WorkflowModule.py
+++ modules/WorkflowModule.py
@@ -1,15 +1,27 @@
+from enum import Enum
 from modules.MiningModule import MiningModule
 
-
+class WorkflowParams(Enum):
+    COUNT = 'count'
+    NAME = 'name'
 class WorkflowModule(MiningModule):
-    def __init__(self, repo):
+    def __init__(self, repo, params):
         self.workflows = repo.get_workflows()
         self.json = {'workflows': {}}
+        self.params = params
 
     def mine(self):
-        self._extract_workflow_count()
-        self._extract_workflow_name()
+        map(self.params, self._extract_param_info())
         return self.json
+
+    def _extract_param_info(self, param):
+        match param:
+            case WorkflowParams.COUNT:
+                self._extract_workflow_count()
+            case WorkflowParams.NAME:
+                self._extract_workflow_name()
+            case _:
+                return "Invalid param for Workflow Module"
 
     def _extract_workflow_count(self):
         self.json['workflows']['count'] = self.workflows.totalCount
