63666b1d981ac1679b3f41558e71f34a4120080a
Patrick <p.j.hibbs@student.tudelft.nl>
2023-05-09 15:33:24 +0200

    Added params support for the commit and pull request module

--- modules/pull_request_module.py
+++ modules/pull_request_module.py
@@ -1,5 +1,8 @@
 """Module for mining pull request information"""
 import dataclasses
+from enum import Enum
+
+from modules.exception import ModuleParamException
 from modules.mining_module import MiningModule
 
 
@@ -19,25 +22,43 @@ class PullRequestModule(MiningModule):
     -------
     mine()
         The main entry point to this class. Calling this function will mine all the data in the body
+    _extract_param_info(param)
+        Calls the right function given a parameter from self.params
     _extract_pull_request_titles()
         Extracts all pull request titles from a repository
     _extract_commit_count()
         Extracts all pull request bodies from a repository
     """
 
-    def __init__(self):
+    def __init__(self, params):
         self.pulls = super().repo.get_pulls(state='all')
         self.json = {'pull_requests': {}}
+        self.params = params
 
     def mine(self):
-        """Mines all the data in the body and returns a dictionary with all the mined data"""
-        self._extract_pull_request_titles()
-        self._extract_pull_request_bodies()
-
+        """Mines all the data in self.params and returns a dictionary with all the mined data"""
+        for param in self.params:
+            self._extract_param_info(param)
         return self.json
+
+    def _extract_param_info(self, param):
+        if param in (PullRequestParams.TITLES, PullRequestParams.TITLES.value):
+            self._extract_pull_request_titles()
+        elif param in (PullRequestParams.BODIES, PullRequestParams.BODIES.value):
+            self._extract_pull_request_bodies()
+        else:
+            raise ModuleParamException("Module does not have param: " + str(param))
 
     def _extract_pull_request_titles(self):
         self.json['pull_requests']['titles'] = [pull.title for pull in self.pulls]
 
     def _extract_pull_request_bodies(self):
         self.json['pull_requests']['bodies'] = [pull.body for pull in self.pulls]
+
+
+class PullRequestParams(Enum):
+    """
+    A class that holds enum values for the functions in the commits_module class
+    """
+    TITLES = 'titles'
+    BODIES = 'bodies'
