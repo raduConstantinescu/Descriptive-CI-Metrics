1fd7fb6eef05da083a0d37973c553584fa0707f2
Radu Constantinescu <Constantinescu-3@student.tudelft.nl>
2023-05-10 10:35:09 +0200

    Extend Workflow Module: ID, CREATION/UPDATE Date

--- modules/workflow_module.py
+++ modules/workflow_module.py
@@ -28,6 +28,12 @@ class WorkflowModule(MiningModule):
         Extracts the number of workflows from a repository
     _extract_workflow_name()
         Extracts the workflow names from a repository
+    _self._extract_workflow_id()
+        Extracts the workflows ids.
+    self._extract_workflow_creation_date()
+        Extracts workflow creation date.
+    self._extract_workflow_update_date()
+        Extracts workflow update date.
     """
 
     # Params, represent the information you want extracted from the Workflow Module.
@@ -48,14 +54,26 @@ class WorkflowModule(MiningModule):
             self._extract_workflow_count()
         elif param in (WorkflowParams.NAME, WorkflowParams.NAME.value):
             self._extract_workflow_name()
+        elif param in (WorkflowParams.ID, WorkflowParams.ID.value):
+            self._extract_workflow_id()
+        elif param in (WorkflowParams.CREATED_AT, WorkflowParams.CREATED_AT.value):
+            self._extract_workflow_creation_date()
+        elif param in (WorkflowParams.UPDATED_AT, WorkflowParams.UPDATED_AT.value):
+            self._extract_workflow_update_date()
         else:
             raise ModuleParamException("Module does not have param: " + str(param))
 
     def _extract_workflow_count(self):
         self.json['workflows']['count'] = self.workflows.totalCount
-
     def _extract_workflow_name(self):
         self.json['workflows']['name'] = [workflow.name for workflow in self.workflows]
+    def _extract_workflow_id(self):
+        self.json['workflows']['id'] = [workflow.id for workflow in self.workflows]
+    # Example of possible date retrieved: "2021-03-03 20:24:01"
+    def _extract_workflow_creation_date(self):
+        self.json['workflows']['created_at'] = [workflow.created_at.strftime("%Y-%m-%d %H:%M:%S") for workflow in self.workflows]
+    def _extract_workflow_update_date(self):
+        self.json['workflows']['updated_at'] = [workflow.updated_at.strftime("%Y-%m-%d %H:%M:%S") for workflow in self.workflows]
 
 
 # Parameters of the Workflow
@@ -65,3 +83,6 @@ class WorkflowParams(Enum):
     """
     COUNT = 'count'
     NAME = 'name'
+    ID = 'id'
+    CREATED_AT = 'created_at'
+    UPDATED_AT = 'updated_at'
