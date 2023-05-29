ffe601e8d542ef5c4528f8e37496e37ce8544142
Bram de Vries <acdevries@home.nl>
2023-05-29 13:38:36 +0200

    Add Repository and Workflow Config Modules (#28)
    * Extend Workflow Module: ID, CREATION/UPDATE Date
    
    * Refactor Workflow Module Json Schema
    
    * Add repository module
    
    * Add workflow config module
    
    * Add check for exception
    
    * Add method for getting commit meta data
    
    ---------
    
    Co-authored-by: Radu Constantinescu <Constantinescu-3@student.tudelft.nl>

---
+++ modules/workflow_config_module.py
@@ -0,0 +1,129 @@
+"""Module for mining workflow config files"""
+import dataclasses
+
+from enum import Enum
+import os
+
+from github import GithubException
+
+from modules.commits_module import CommitParams, CommitsModule
+from modules.mining_module import MiningModule
+from modules.exception import ModuleParamException
+
+@dataclasses.dataclass
+class WorkflowConfigModule(MiningModule):
+    """
+    This class mines workflow config .yml files
+
+    Attributes
+    ----------
+    json : dict
+        Dictionary containing information about the workflow files
+    params: list
+        A list that contains what information you want from the repository in this module.
+
+    Methods
+    -------
+    mine()
+        The main entry point to this class. Calling this function will mine all the data in
+        self.params.
+    _extract_github_actions_config()
+        Extracts the GitHub actions workflow config files from a repository
+    _extract_travis_ci_config()
+        Extracts the TravisCI workflow config files from a repository
+    _is_yml_file()
+        Checks whether a filename ends with ".yml" or ".yaml"
+    _store_config_file()
+        Saves a workflow config file to a "out/{repo_name}/{ci_platform}" folder
+    """
+
+    # Params, represent the information you want extracted from the Workflow Module.
+    # Default behaviour: no params passed: all workflow information extracted.
+    def __init__(self, params=None):
+        self.json = {
+                        'workflow_files': [],
+                        'workflow_platforms': {
+                            'github_actions': True,
+                            'travis_ci': True
+                        }
+                    }
+        self.params = [c.value for c in WorkflowConfigParams] if params is None else params
+
+    def mine(self):
+        for param in self.params:
+            self._extract_param_info(param=param)
+        return self.json
+
+    # Maps Workflow Param Enum to Param Extractor Function
+    def _extract_param_info(self, param):
+        if param in (WorkflowConfigParams.GITHUB_ACTIONS_CONFIG, WorkflowConfigParams.GITHUB_ACTIONS_CONFIG.value):
+            self._extract_github_actions_config()
+        elif param in (WorkflowConfigParams.TRAVIS_CI_CONFIG, WorkflowConfigParams.TRAVIS_CI_CONFIG.value):
+            self._extract_travis_ci_config()
+        elif param in (WorkflowConfigParams.COMMITS, WorkflowConfigParams.COMMITS.value):
+            self._extract_commits()
+        else:
+            raise ModuleParamException("Module does not have param: " + str(param))
+
+    def _extract_github_actions_config(self):
+        try:
+            contents = super().repo.get_contents(".github/workflows")
+
+            while contents:
+                config_file = contents.pop(0)
+
+                if self._is_yml_file(config_file.name):
+                    self.json['workflow_files'].append({ 'platform': 'github_actions', 'path': config_file.path})
+                    self._store_config_file('github-actions', config_file)
+        except GithubException:
+            self.json['workflow_platforms']['github_actions'] = False
+
+
+    def _extract_travis_ci_config(self):
+        try:
+            contents = super().repo.get_contents("")
+            travis_ci_detected = False
+
+            while contents and not travis_ci_detected:
+                config_file = contents.pop(0)
+
+                if (self._is_yml_file(config_file.name) and config_file.name.startswith('.travis')):
+                    travis_ci_detected = True
+                    self.json['workflow_files'].append({ 'platform': 'travis_ci', 'path': config_file.path})
+                    self._store_config_file('travis-ci', config_file)
+
+            if not travis_ci_detected:
+                self.json['workflow_platforms']['travis_ci'] = False
+
+        except GithubException:
+            self.json['workflow_platforms']['travis_ci'] = False
+
+    def _extract_commits(self):
+        for file in self.json['workflow_files']:
+            print(file)
+            commit_module = CommitsModule([CommitParams.COMMIT_META], path=file['path'])
+            commits = commit_module.mine()
+            file['commits'] = commits['commits']['meta']
+
+
+    def _is_yml_file(self, filename):
+        return filename.endswith('.yml') or filename.endswith('.yaml')
+
+    def _store_config_file(self, platform_name, config_file):
+        out_filename = "out/" + super().repo.name + "/" + platform_name + "/" + config_file.name
+
+        os.makedirs(os.path.dirname(out_filename), exist_ok=True)
+
+        with open(out_filename, "w", encoding="utf-8") as out_file:
+            out_file.write(config_file.decoded_content.decode("utf-8"))
+            out_file.close()
+
+
+# Parameters of the Workflow Config
+class WorkflowConfigParams(Enum):
+    """
+    A class that holds enum values for the functions in the WorkflowConfigModule class
+    """
+    GITHUB_ACTIONS_CONFIG = 'github_actions_config'
+    TRAVIS_CI_CONFIG = 'travis_ci_config'
+    COMMITS = 'commits'
--- modules/workflow_module.py
+++ modules/workflow_module.py
@@ -8,7 +8,7 @@ from modules.exception import ModuleParamException
 @dataclasses.dataclass
 class WorkflowModule(MiningModule):
     """
-    This class mines commit information
+    This class mines workflow information
 
     Parameters
     ----------
@@ -33,34 +33,57 @@ class WorkflowModule(MiningModule):
         Extracts the number of workflows from a repository
     _extract_workflow_name()
         Extracts the workflow names from a repository
+    _self._extract_workflow_id()
+        Extracts the workflows ids.
+    self._extract_workflow_creation_date()
+        Extracts workflow creation date.
+    self._extract_workflow_update_date()
+        Extracts workflow update date.
     """
 
     # Params, represent the information you want extracted from the Workflow Module.
+    # Default behaviour: no params passed: all workflow information extracted.
     def __init__(self, repo, params=None):
         self.workflows = repo.get_workflows()
-        self.json = {'workflows': {}}
+        self.json = {'workflow_count' : {},
+                     'workflows': {}
+                     }
         self.params = [c.value for c in WorkflowParams] if params is None else params
 
     def mine(self):
         """Mines all the data in self.params and returns a dictionary with all the mined data"""
-        for param in self.params:
-            self._extract_param_info(param=param)
+        self._extract_workflow_count()
+        for workflow in self.workflows:
+            workflow_data = {}
+            for param in self.params:
+                self._extract_param_info(param=param, workflow=workflow, workflow_data=workflow_data)
+            self.json['workflows'][workflow.id] = workflow_data
         return self.json
 
     # Maps Workflow Param Enum to Param Extractor Function
-    def _extract_param_info(self, param):
-        if param in (WorkflowParams.COUNT, WorkflowParams.COUNT.value):
-            self._extract_workflow_count()
-        elif param in (WorkflowParams.NAME, WorkflowParams.NAME.value):
-            self._extract_workflow_name()
+    def _extract_param_info(self, param, workflow, workflow_data):
+        if param in (WorkflowParams.NAME, WorkflowParams.NAME.value):
+            self._extract_workflow_name(workflow, workflow_data)
+        elif param in (WorkflowParams.ID, WorkflowParams.ID.value):
+            self._extract_workflow_id(workflow, workflow_data)
+        elif param in (WorkflowParams.CREATED_AT, WorkflowParams.CREATED_AT.value):
+            self._extract_workflow_creation_date(workflow, workflow_data)
+        elif param in (WorkflowParams.UPDATED_AT, WorkflowParams.UPDATED_AT.value):
+            self._extract_workflow_update_date(workflow, workflow_data)
         else:
             raise ModuleParamException("Module does not have param: " + str(param))
 
-    def _extract_workflow_count(self):
-        self.json['workflows']['count'] = self.workflows.totalCount
 
-    def _extract_workflow_name(self):
-        self.json['workflows']['name'] = [workflow.name for workflow in self.workflows]
+    def _extract_workflow_count(self):
+        self.json['workflow_count'] = self.workflows.totalCount
+    def _extract_workflow_name(self, workflow, workflow_data):
+        workflow_data['name'] = workflow.name
+    def _extract_workflow_id(self, workflow, workflow_data):
+        workflow_data['id'] = workflow.id
+    def _extract_workflow_creation_date(self, workflow, workflow_data):
+        workflow_data['created_at'] = workflow.created_at.strftime("%Y-%m-%d %H:%M:%S")
+    def _extract_workflow_update_date(self, workflow, workflow_data):
+        workflow_data['updated_at'] = workflow.updated_at.strftime("%Y-%m-%d %H:%M:%S")
 
 
 # Parameters of the Workflow
@@ -68,5 +91,7 @@ class WorkflowParams(Enum):
     """
     A class that holds enum values for the functions in the WorkflowModule class
     """
-    COUNT = 'count'
     NAME = 'name'
+    ID = 'id'
+    CREATED_AT = 'created_at'
+    UPDATED_AT = 'updated_at'
